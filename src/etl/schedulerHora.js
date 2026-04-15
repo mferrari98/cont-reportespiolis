8e732d0a55df6a1aba3582c8e062fe0d6a5b8522
mferrari <mferrari@debian.servicoop.com>
2026-04-15 11:19:41 -0300

    test: add scheduler utilities for hourly ingestion

---
+++ src/etl/schedulerHora.js
@@ -0,0 +1,34 @@
+function getNextTopOfHour(now = new Date()) {
+  const nextTop = new Date(now);
+  nextTop.setMinutes(0, 0, 0);
+  nextTop.setHours(nextTop.getHours() + 1);
+  return nextTop;
+}
+
+function msUntilNextTopOfHour(now = new Date()) {
+  if (
+    now.getMinutes() === 0 &&
+    now.getSeconds() === 0 &&
+    now.getMilliseconds() === 0
+  ) {
+    return 0;
+  }
+
+  return getNextTopOfHour(now).getTime() - now.getTime();
+}
+
+function buildRetryDelaysMs(retryDelaysSeconds, firstAttemptDelaySeconds) {
+  const delaysMs = retryDelaysSeconds.map((delaySeconds) => delaySeconds * 1000);
+
+  if (typeof firstAttemptDelaySeconds === 'number' && firstAttemptDelaySeconds >= 0) {
+    return [0, ...delaysMs];
+  }
+
+  return [0, ...delaysMs];
+}
+
+module.exports = {
+  getNextTopOfHour,
+  msUntilNextTopOfHour,
+  buildRetryDelaysMs,
+};
